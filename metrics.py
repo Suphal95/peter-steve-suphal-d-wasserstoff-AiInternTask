fcc5aa4b10a42e07d03b82ceb30ba9c6316b3774
Suphal95 <peter1steve@gmail.com>
2024-11-14 14:39:42 +0530

    added error logging

--- metrics.py
+++ metrics.py
@@ -1,27 +1,64 @@
 import psutil
 import time
 import threading
+import logging
 from functools import wraps
+
+## setup logging configuration
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 ## function to get active thread count
 def getActiveThreadCount():
-    return threading.active_count()
+    """
+    returns active thread count currently in, used by the program.
+    """
+    try:
+        activeCount = threading.active_count()
+        logging.info(f"Current active thread count: {activeCount}".)
+        return activeCount
+    
+    except Exception as e:
+        logging.error(f"Error getting active thread count: {e}")
+        return 0
+    
 
 ## function to log current resource usage(cpu and memory)
 def logResourceUsage():
-    cpu_percent = psutil.cpu_percent(interval=1) ## get cpu usage
-    memory_info = psutil.virtual_memory() ## get memory usage
-    print(f"CPU Usage: {cpu_percent}%")
-    print(f"Memory Usage: {memory_info.percent}%")
+    """
+    returns the cpu and the memory usage
+    """
+    try:
+        ## get cpu usage
+        cpuPercent = psutil.cpu_percent(interval=1) ## get cpu usage
+        logging.info(f"CPU Usage: {cpuPercent}%")
+        
+        ## get memory usage
+        memoryInfo = psutil.virtual_memory() ## get memory usage
+        logging.info(f"Memory Usage: {memoryInfo.percent}%")
+    
+    ## error logging
+    except Exception as e:
+        logging.error(f"Error in logging resorce usage: {e}.")
+
 
 ## function to track execution time()
 def trackExecutionTime(func):
+    """
+    this is a decorator function used to check execution tim eof a function.
+    example(how to use):
+    @trackExecutionTime ## call fucntion first
+    def myFunction():
+        my function code
+    
+    myFunction() ## time will get logged because @trackExecutionTime is added before creating myFunction.
+    """
     @wraps(func) ## using the wraps from functools
     def wrapper(*arge,**kwargs):
         start_time = time.time() ## start time before function
         result = func(*arge,**kwargs) ## call original function
         end_time = time.time() ## end time after function finished
         execution_time = end_time-start_time
-        print(f"{func.__name__} processed in {execution_time:.2f} seconds.") ## print execution time
+        ## logging execution time
+        logging.info(f"{func.__name__} processed in {execution_time:.2f} seconds.") ## print execution time
         return result ## return the funnction result
     return wrapper
